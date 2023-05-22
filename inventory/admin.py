878493b05f1f23f1a91f8e8258311f894ebc83c6
Ulgamon <ilicjustin@gmail.com>
2023-05-22 12:50:03 +0200

    c2

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,2 +1,21 @@
 from django.contrib import admin
+from .models import Product, Brand, Category, Stock
 
+class BrandAdmin(admin.ModelAdmin):
+    model = Brand
+    list_display = (
+        "name",
+        "brand_id",
+    )
+
+class StockAdmin(admin.ModelAdmin):
+    model = Stock
+    list_display = (
+        "product",
+        "units",
+    )
+
+admin.site.register(Product)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(Category)
+admin.site.register(Stock, StockAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,14 +1,38 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural="Categories"
+
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    qty = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        ordering = ["age"]
 
     def __str__(self):
         return self.name
 
 class Brand(models.Model):
+    brand_id = models.BigAutoField(primary_key=True, )
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
+class Stock(models.Model):
+    units = models.BigIntegerField()
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
+    
